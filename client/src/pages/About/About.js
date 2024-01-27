15130a0f2a663f545d4d7477444955e3bbe15693
Aditya Singh <as23july@gmail.com>
2024-01-28 03:39:42 +0530

    contact changes

---
+++ client/src/pages/About/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./About.css";
+import profilePic from "../../assets/images/About_my.jpg";
+import Jump from 'react-reveal/Jump';
+
+const About = () => {
+    return (
+        <>
+            <Jump>
+            <div class="about">
+                <div class="row">
+                    <div class="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-img">
+                        <img className="" src={profilePic} alt="profile_pic" />
+                    </div>
+                    <div class="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
+                        <h1>About Me</h1>
+                        <p>
+                            An enthusiastic and highly motivated fresher in Computer Science Engineering and wants to work in a firm with a professional work driven environment to utilize and apply my knowledge and skills which would enable me as a fresh graduate to grow while fulfilling organizational goals. </p>
+                    </div>
+                </div>
+            </div>
+            </Jump>
+        </>
+    )
+}
+export default About
