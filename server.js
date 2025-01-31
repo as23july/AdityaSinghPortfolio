34b3187507d345522a96cc5fa4be08db425e92bc
Aditya Singh <as23july@gmail.com>
2025-02-01 04:37:37 +0530

    Resume Updated

--- server.js
+++ server.js
@@ -1,30 +1,29 @@
-const express = require('express')
-const cors = require('cors')
-const dotenv = require('dotenv')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// static files 
-app.use(express.static(path.join(__dirname, './client/build')))
+// static files
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
-app.use('/api/v1/portfolio', require('./routes/portfolioRoutes'));
-app.get('*', function(req, res){
-    res.sendFile(path.join(__dirname, './client/build/index.html'));
-}); 
-
+app.use("/api/v1/portfolio", require("./routes/portfolioRoutes"));
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
 
 // port
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8080;
 
 // listen
 app.listen(PORT, () => {
-    console.log(`Server Running on PORT ${PORT} `);
-})
+  console.log(`Server Running on PORT ${PORT} `);
+});
